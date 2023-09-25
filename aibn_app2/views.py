da75852785dd629ad9d24638355647b2d7162d2c
Alfredo Balboa <alfredonachobalboa@gmail.com>
2023-09-25 15:20:03 -0300

    app 2 agregada y vistas completas

--- aibn_app2/views.py
+++ aibn_app2/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def vista3(request):
+    html = """
+    <style>
+    h1 {
+        background-color: black;
+        color: black;
+    }
+    h1:hover {
+        color: white;
+    }
+    img {
+        width: 200px;
+        height: auto;
+    }
+    </style>
+    <h1>esta es la vista 3</h1>
+    <img src="https://i.kinja-img.com/gawker-media/image/upload/s--zuGpIoEo--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/arcpyf4oh7wtrwlbjn04.jpg" alt="aqui deberia haber una imagen xd">
+    <br>
+    <input type="text">
+    """
+    return HttpResponse(html)
+
+def vista4(request):
+    html = """
+    <style>
+    h1 {
+        background-color: black;
+        color: black;
+    }
+    h1:hover {
+        color: white;
+    }
+    img {
+        width: 200px;
+        height: auto;
+    }
+    </style>
+    <h1>esta es la vista 4</h1>
+    <img src="https://th.bing.com/th/id/OIP.dQO-VTn5GVuANksPgEBxgAHaKK?pid=ImgDet&rs=1" alt="aqui deberia haber una imagen xd">
+    <br>
+    <input type="text">
+    """
+    return HttpResponse(html)
--- aibn_proyecto1/settings.py
+++ aibn_proyecto1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aibn_app2',
 ]
 
 MIDDLEWARE = [
